bca66db45f10a32dd421b61ae8f63f5dc447a5cd
Jerome Kaphael Ghartey <gharteyjerome@gmail.com>
2025-02-06 00:16:54 +0000

    Config

--- next.config.mjs
+++ next.config.mjs
@@ -18,28 +18,13 @@ const config = {
     // During development you may want to set this to false
     ignoreDuringBuilds: true,
   },
-  output: "standalone",
+  output: "export",
+  distDir: ".next",
+  images: {
+    unoptimized: true,
+  },
   swcMinify: true,
   poweredByHeader: false,
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          { key: "X-DNS-Prefetch-Control", value: "on" },
-          { key: "X-XSS-Protection", value: "1; mode=block" },
-          { key: "X-Frame-Options", value: "SAMEORIGIN" },
-          { key: "X-Content-Type-Options", value: "nosniff" },
-        ],
-      },
-    ];
-  },
-  async redirects() {
-    return [];
-  },
-  async rewrites() {
-    return [];
-  },
 };
 
 export default config;
